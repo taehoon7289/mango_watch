3bb30e51198858694718bbd276713819b0b914a9
taehoon <taehoon7289@gmail.com>
2021-09-25 15:47:12 +0900

    feat: 학습
    - construct 추가

--- lib/page_view/page_view_1.dart
+++ lib/page_view/page_view_1.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class PageView1 extends StatelessWidget {
+  const PageView1({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/page_view/page_view_2.dart
+++ lib/page_view/page_view_2.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class PageView2 extends StatelessWidget {
+  const PageView2({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
